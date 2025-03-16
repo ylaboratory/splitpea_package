6398073868e4125f1d648faf9ee12432426e2adb
Jeffrey Zhong <jz94@mochi.cs.rice.edu>
2025-03-16 00:14:53 -0500

    fixed error message

--- splitpea/main.py
+++ splitpea/main.py
@@ -166,7 +166,7 @@ def run(in_file,
 
     if input_format.lower() == "suppa2":
         if not (isinstance(in_file, (list, tuple)) and len(in_file) == 2):
-            raise ValueError("For SUPPA2 input format, in_file must be a list or tuple of 2 file paths: [psivec_file,  ].")
+            raise ValueError("For SUPPA2 input format, in_file must be a list or tuple of 2 file paths: [psivec_file, dpsi_file].")
         psivec_file, dpsi_file = in_file
         
         if psivec_file.endswith(".dpsi") and dpsi_file.endswith(".psivec"):
